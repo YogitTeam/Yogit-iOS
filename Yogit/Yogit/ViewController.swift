42381019752389fd6763552b61ff6a0e5f8abc94
devjohnpark <devjohnpark@gmail.com>
2022-09-06 15:46:16 +0900

    Add project file

---
+++ Yogit/Yogit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yogit
+//
+//  Created by Junseo Park on 2022/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
