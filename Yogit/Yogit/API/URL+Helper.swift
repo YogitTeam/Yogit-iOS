fda7c4e05090d72841bd9125db0c61fb0c87e292
devjohnpark <devjohnpark@gmail.com>
2022-09-28 23:08:27 +0900

    Add URL+Helper

--- Yogit/Yogit/API/URL+Helper.swift
+++ Yogit/Yogit/API/URL+Helper.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+// URLComponents
+// 정확한 문자열 구성 어려움 > URLComponents을 이용해서 쿼리를 구성
+// 쿼리 받게 extension
+// 파라메터로 넘어온 모든 딕션너리를 url queryitem instance로 변경이 핵심
 extension URL {
     func withQueries(_ queries: [String: String]) -> URL? {
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true) // url 자체를 받고 baseurl 기반으로 쓰도록
